b61b485b82236d1ed031bcf444baee403d80eac6
Vernon Roberts <nakkiasr@live.com>
2021-09-22 21:44:51 -0700

    Created AI player.

---
+++ RSPLSProject/AIplayer.cs
@@ -0,0 +1,19 @@
+public class AIplayer ();
+{
+    // member variables (What does the class HAVE? The class HAS...)
+    public string AIplayer();
+
+//constructor (unique methods/functions) - declaring a variable scoped to the constructor. Set up any default values (if they make sense)
+public AIplayer(string name, string gesture, int score) //
+{
+    this.name = "Computer";
+}
+
+//member methods - actions that can be performed with the object (What CAN the object do? The object CAN...)
+public string ChooseGesture();
+{
+    Console.WriteLine("The Computer chose " + "" + gesture ".");
+}
+    }
+}
+}
